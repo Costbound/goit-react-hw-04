0d7c3ed9e20eaaef0c4e6178e427e53b014e1cc9
Costbound <popovskiy96@gmail.com>
2024-04-04 18:57:48 +0000

    search bar done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,15 @@
 import './App.module.css'
+import SearchBar from '../SearchBar/SearchBar'
 
 export default function App() {
+
+    const handleSubmit = (searchValue) => {
+
+    }
+
     return (
         <div className="content-container">
-            
+            <SearchBar onSubmit={handleSubmit}/>
         </div>
     )
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,37 @@
-export default function SearchBar() {
+import css from './SearchBar.module.css'
+import { AiOutlineSearch } from "react-icons/ai";
+import toast, { Toaster } from 'react-hot-toast'
+
+export default function SearchBar({ onSubmit }) {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const value = e.target.search.value
+        if (!value.trim().length) {
+            return toast.error("This field cannot be empty")
+        }
+        onSubmit(value)
+    }
+
     return (
-        <></>
+        <header className={css.header}>
+            <form className={css.form} onSubmit={handleSubmit}>
+                <input
+                    className={css.searchInput}
+                    name='search'
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                />
+                <button className={css.submitButton} type="submit">
+                    <AiOutlineSearch
+                        className={css.searchIcon}
+                        size='20'
+                    />
+                </button>
+                <Toaster containerStyle={{ top: 60 }} />
+            </form>
+        </header>
     )
 }
